799ba91919e6fa744e049e38d040a0e19e16757c
KIrina11 <90615217+KIrina11@users.noreply.github.com>
2021-10-21 21:20:29 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,18 +7,21 @@ public class Main {
         int sum =0;
 
         for (int i = 100; i <= 999; i++) {
+            
             int digit1 = i / 100;
             int digit2 = i / 10 % 10;
             int digit3 = i % 10;
             
             sum = calculateSum(i, digit1, digit2, digit3, sum);
         }
+        
         writeArea(sum);
     }
 
     private static int calculateSum(int i, int digit1, int digit2, int digit3, int sum) {
         
         if (digit1 != digit2 && digit1 != digit3 && digit2 != digit3) {
+            
             sum = sum + i;
             return sum;
         } 
@@ -28,7 +31,6 @@ public class Main {
     }
 
     private static void writeArea(int sum) {
-        
         System.out.printf("The sum of the numbers from 100 to 999,"
                 + " such that in each number all the digits are different = %d",sum);
     }
